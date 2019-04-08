176a64b981843c55477a7383da7eecd407a89f96
Olivia Meiring <olivia@yummy-wakame.com>
2019-04-08 10:21:10 -0600

    past dailies structure added

---
+++ client/src/components/pages/PastDailies.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class PastDailies extends Component {
+    
+    render() {
+        return (
+            <main id="page-wrap">
+                <h2>Past Daily Readings</h2>
+
+            </main>
+        )
+    }
+}
+
+export default PastDailies
